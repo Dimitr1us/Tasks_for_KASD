8cc16002cca0405365702006f626b326da7c05f1
Professional <speed.Dima05@gmail.com>
2024-09-27 19:07:28 +0300

    Creation of Task4

---
+++ Task4/Task4/Program.cs
@@ -0,0 +1,5 @@
+﻿using MyArrayListLibrary;
+Console.WriteLine("авааав");
+string[] array = { "a","b","c"};
+MyArrayList <string> myArrayList = new MyArrayList<string> (array);
+Console.WriteLine($"{myArrayList.contains("a")}");
