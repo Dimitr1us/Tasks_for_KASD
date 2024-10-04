a913aa9cc5b50d6ab3a6744ce2f335847c7ad699
Professional <speed.Dima05@gmail.com>
2024-10-04 21:37:31 +0300

    Creation of Task8

---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyStackLibrary;
+MyStack<int> stack = new MyStack<int>();
+Console.WriteLine(stack.empty());
+stack.push(1);
+stack.push(10);
+stack.push(20);
+stack.push(5);
+Console.WriteLine(stack.search(1));
+Console.WriteLine(stack.empty());
+
+
