3fa3fbd526b7458f29f97181fccefdf1c2915944
Professional <speed.Dima05@gmail.com>
2024-10-24 22:33:19 +0300

    Creation of Task13

---
+++ Task13/Task3_3/Interface1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparatorLib
+{
+    public interface IComparator<T>
+    {
+        int Compare(T x, T y);
+    }
+
+    public class Comparator : IComparator<int>
+    {
+        public int Compare(int x, int y)
+        {
+            string strX = x.ToString();
+            string strY = y.ToString();
+
+            // Сравниваем длины строк
+            if (strX.Length > strY.Length)
+            {
+                return 1; // x длиннее, возвращаем 1
+            }
+            else if (strX.Length < strY.Length)
+            {
+                return -1; // y длиннее, возвращаем -1
+            }
+            else
+            {
+                return 0; // Длину одинаковая, возвращаем 0
+            }
+        }
+    }
+}
