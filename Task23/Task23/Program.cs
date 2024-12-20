cbb0081e5295b9f2e3405651bf65666ceb1b1b85
Professional <speed.Dima05@gmail.com>
2024-12-20 16:13:30 +0300

    Creation of Task23

---
+++ Task23/Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿using MyHashSetLib;
+MyHashSet<int> myHashSet = new MyHashSet<int>();
+myHashSet.add(1, 2);
+myHashSet.add(3, 4);
+myHashSet.add(5, 6);
+myHashSet.add(6, 7);
+myHashSet.remove(1);
+Console.WriteLine(myHashSet.first());
